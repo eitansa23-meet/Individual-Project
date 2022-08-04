483807cf91bb76d6c1800cc9c7407c060e4c7731
eitansa23-meet <eitaneitan121@gmail.com>
2022-08-04 17:31:16 +0300

    123456

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -31,7 +31,8 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 #Code goes below here
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template("final.html")
+    if 'user' in login_session:
+        return render_template("final.html")
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
